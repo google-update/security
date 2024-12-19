7814fa18a0c98011fb2265e6132c784fc2193cf9
google-update <marielipanesar@gmail.com>
2024-12-19 23:19:55 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,1 @@
-defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1(805) 608-3883  to unlock it!';
+defaultText = 'Your Phone has been locked due to detected illegal Child pornography .Your Google Account  has been disabled . on! Immediately call Online Security Support +1(833) 365-6141  to unlock it!';
